456056e9931ffae4fe455cd648962475e33cd357
Vaduva Paula <paula.alexandra.vaduva@gmail.com>
2025-11-22 00:02:44 +0200

    edit-repositories

--- Repositories/OrderRepository.cs
+++ Repositories/OrderRepository.cs
@@ -1,4 +1,5 @@
-﻿using PetShop.Context;
+﻿using Microsoft.EntityFrameworkCore;
+using PetShop.Context;
 using PetShop.Models;
 using PetShop.Repositories.Interfaces;
 
@@ -7,5 +8,24 @@ namespace PetShop.Repositories
     public class OrderRepository : RepositoryBase<Order>, IOrderRepository
     {
         public OrderRepository(PetShopContext context) : base(context) { }
+
+        public string? GetUserEmailById(string userId)
+        {
+            return PetShopContext.Users
+                .Where(u => u.Id == userId)
+                .Select(u => u.Email)
+                .FirstOrDefault();
+        }
+
+        public Order? GetFinishedOrderById(int id)
+        {
+            return PetShopContext.Orders
+                .Include(o => o.OrderItems)
+                    .ThenInclude(oi => oi.Product)
+                .Where(o => o.statusOrder == "Finished" && o.Id == id)
+                .AsEnumerable()
+                .Where(o => o.OrderItems.Any(oi => oi.Product != null))
+                .FirstOrDefault();
+        }
     }
 }
--- Repositories/ProductRepository.cs
+++ Repositories/ProductRepository.cs
@@ -1,4 +1,5 @@
-﻿using PetShop.Context;
+﻿using Microsoft.EntityFrameworkCore;
+using PetShop.Context;
 using PetShop.Models;
 using PetShop.Repositories.Interfaces;
 
@@ -7,5 +8,30 @@ namespace PetShop.Repositories
     public class ProductRepository : RepositoryBase<Product>, IProductRepository
     {
         public ProductRepository(PetShopContext context) : base(context) { }
+
+        public Product GetById(int id)
+        {
+            return PetShopContext.Products
+                .Include(c => c.Category)
+                .Include(o => o.OrderItems)
+                .FirstOrDefault(m => m.Id == id);
+        }
+
+        public bool ProductExists(int id)
+        {
+            return PetShopContext.Products.Any(m => m.Id == id);
+        }
+
+        public List<OrderItem> GetAllOrderItems()
+        {
+            return PetShopContext.OrderItems.ToList();
+        }
+
+        public Category GetProductByCategory(int categoryId)
+        {
+            return PetShopContext.Categories
+                .Include(a => a.Products)
+                .FirstOrDefault(c => c.Id == categoryId);
+        }
     }
 }
