c9c66212a4ca2ecaf3ba0bd8519b4d6ef0af853c
Vaduva Paula <paula.alexandra.vaduva@gmail.com>
2025-11-23 00:36:48 +0200

    add-dtos

---
+++ DTOs/GroupCategoryDto.cs
@@ -0,0 +1,10 @@
+﻿namespace PetShop.DTOs
+{
+    public class GroupCategoryDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<ProductDto> Products { get; set; }
+        public ICollection<string> ProductName { get; set; }
+    }
+}
---
+++ DTOs/GroupProductDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PetShop.DTOs
+{
+    public class GroupProductDto
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public float Price { get; set; }
+        public byte[]? ProductImage { get; set; }
+        [DataType(DataType.Upload)]
+        [DisplayName("Imagine")]
+        [NotMapped]
+        public IFormFile ImageFile { get; set; }
+        [Required]  
+        public int? Stock {  get; set; }
+    }
+}
