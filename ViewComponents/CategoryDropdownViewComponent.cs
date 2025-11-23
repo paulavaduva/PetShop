e6e44235c6192df7029f7ed36fd2771fe216017e
Vaduva Paula <paula.alexandra.vaduva@gmail.com>
2025-11-23 19:04:08 +0200

    add category component and view by category

---
+++ ViewComponents/CategoryDropdownViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetShop.Services.Interfaces;
+
+namespace PetShop.ViewComponents
+{
+    public class CategoryDropdownViewComponent : ViewComponent
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryDropdownViewComponent(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var categories = _categoryService.GetAllCategories();
+            return View(categories);
+        }
+    }
+}
