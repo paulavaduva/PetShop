e1b76507527afabd669788a4f4f6c4a4abde1af0
Vaduva Paula <paula.alexandra.vaduva@gmail.com>
2025-12-30 23:59:20 +0200

    add role authorization

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using PetShop.Models;
 
@@ -13,6 +14,7 @@ namespace PetShop.Controllers
             _logger = logger;
         }
 
+        [AllowAnonymous]
         public IActionResult Index()
         {
             return View();
